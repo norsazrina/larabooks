a2525ae202382f0960575f8b34dc2d55946253bf
norsazrina <norsazrina@uitm.edu.my>
2025-10-16 12:57:18 +0800

    modified

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,5 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
-	protected $fillable = ['title'];
+	
+	protected $fillable = ['title', 'book_category_id'];
+
+	public function category()
+	{
+		return $this->belongsTo(BookCategory::class, 'book_category_id');
+	}
+
 }
